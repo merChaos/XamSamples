ae078bd3a1c7d40f612a83c80b8af0a81c64119a
merchos <rastham.ka@gmail.com>
2014-09-30 08:22:15 +0530

    local db support

---
+++ XamProjRef/XamProjRef/View/RecipeView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamProjRef1.Model;
+using XamProjRef1.ViewModel;
+
+namespace XamProjRef1.View
+{
+    public partial class RecipeView
+    {
+        public RecipeView()
+        {
+            InitializeComponent();
+        }
+
+        public RecipeView(Recipe recipe)
+        {
+            InitializeComponent();
+            this.BindingContext = recipe;
+        }
+        //public RecipeView(RecipeViewModel recipeViewModel)
+        //{
+        //    InitializeComponent();
+        //    this.BindingContext = recipeViewModel;
+        //}
+    }
+}
