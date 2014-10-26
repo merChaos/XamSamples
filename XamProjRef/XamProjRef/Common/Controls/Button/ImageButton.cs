363cc1d4a1782e082a0cbceb6bc49879546dda67
merchos <rastham.ka@gmail.com>
2014-10-26 18:52:52 +0530

    Custom renderer

---
+++ XamProjRef/XamProjRef/Common/Controls/Button/ImageButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamProjRef1.Common.Controls.Button
+{
+    public class ImageButton : Xamarin.Forms.Button
+    {
+        public string BackgroundImageName { get; set; }
+
+        public string ButtonIconName { get; set; }
+
+        public string IconPosition { get; set; }
+
+    }
+}
